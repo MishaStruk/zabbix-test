938cf3aa78141dd72714a015c51b48554e74f6fc
MichaelStruk <michael.struck@devalore.com>
2023-01-10 11:30:16 +0200

    test

--- server.js
+++ server.js
@@ -6,24 +6,43 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 
 var PORT = process.env.port || 8080;
 
-const LaunchRequestHandler = {
+// const LaunchRequestHandler = {
+//     canHandle(handlerInput) {
+//         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+//     },
+//     handle(handlerInput) {
+//         const speechText = 'Hello World - Your skill has launched';
+//         consolg.log("Got here ")
+//         console.log(handlerInput.requestEnvelope);
+//         return handlerInput.responseBuilder
+//             .speak(speechText)
+//             .reprompt(speechText)
+//             .withSimpleCard('Hello World', speechText)
+//             .getResponse();
+//     }
+// };
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
     canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
     },
     handle(handlerInput) {
-        const speechText = 'Hello World - Your skill has launched';
-        consolg.log("Got here ")
-        console.log(handlerInput.requestEnvelope);
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
         return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText)
-            .withSimpleCard('Hello World', speechText)
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
 
 skillBuilder.addRequestHandlers(
-    LaunchRequestHandler
+    IntentReflectorHandler
 )
 
 const skill = skillBuilder.create();
